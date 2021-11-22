85ff7d4de56a12b8eb79c4aa20e34b02fe132bdb
kbroniek <karol.broniek.gtm@gmail.com>
2021-11-22 16:38:28 +0100

    Add forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,18 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
-      HeroSearchComponent
-   ],
+    HeroSearchComponent,
+    HeroFormComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
